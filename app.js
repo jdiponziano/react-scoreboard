ee6a8022d526d4e1abdc61edcba27a3d83308ec2
Jessica DiPonziano <jess@webdevjd.com>
2019-08-05 17:38:22 -0400

    Initial commit of project files

---
+++ app.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+};
+
+ReactDOM.render(<Header />, document.getElementById("root"));
