0b188c803fb6f432aab2485c97ec40cc1f3db121
Jessica DiPonziano <jess@webdevjd.com>
2019-08-05 17:47:37 -0400

    Add counter component

--- app.js
+++ app.js
@@ -15,4 +15,14 @@ const Player = () => {
   );
 };
 
-ReactDOM.render(<Player />, document.getElementById("root"));
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Header />, document.getElementById("root"));
