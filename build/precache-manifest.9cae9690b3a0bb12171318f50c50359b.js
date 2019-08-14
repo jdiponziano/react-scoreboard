7aa4732320e6cf96d3dbd9a5b21d2912e07c00f1
Jessica DiPonziano <jess@webdevjd.com>
2019-08-14 15:10:56 -0400

    Add build files

---
+++ build/precache-manifest.9cae9690b3a0bb12171318f50c50359b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9190cd3b15a31fef7c5da6b7897a0ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82cfba595216620ef3d8",
+    "url": "/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "c6711347337c0bdbd6ef",
+    "url": "/static/js/2.c0c080c6.chunk.js"
+  },
+  {
+    "revision": "82cfba595216620ef3d8",
+    "url": "/static/js/main.ca1be160.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
