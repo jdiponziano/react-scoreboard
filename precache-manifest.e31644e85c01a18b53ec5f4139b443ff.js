4c11b1561783c36db505af705337f329358b9478
Jessica DiPonziano <jess@webdevjd.com>
2019-08-14 15:17:02 -0400

    Updates

---
+++ precache-manifest.e31644e85c01a18b53ec5f4139b443ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350aab8c9fbcfea62fad4fd7812fe202",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "a80233d050e7f4564465",
+    "url": "/react-scoreboard/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "c6711347337c0bdbd6ef",
+    "url": "/react-scoreboard/static/js/2.c0c080c6.chunk.js"
+  },
+  {
+    "revision": "a80233d050e7f4564465",
+    "url": "/react-scoreboard/static/js/main.4610a1ac.chunk.js"
+  },
+  {
+    "revision": "da4941b842451d92ab2c",
+    "url": "/react-scoreboard/static/js/runtime~main.d9728706.js"
+  }
+]);
